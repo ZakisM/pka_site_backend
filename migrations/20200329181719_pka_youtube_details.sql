94d49dca0361a967e5c440d6f3e154fc430edff2
zak <8143258+ZakisM@users.noreply.github.com>
2025-10-04 16:48:42 +0100

    Update migrations

---
+++ migrations/20200329181719_pka_youtube_details.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS pka_youtube_details
+(
+    video_id       TEXT    NOT NULL,
+    episode_number REAL    NOT NULL,
+    title          TEXT    NOT NULL,
+    length_seconds INTEGER NOT NULL,
+    PRIMARY KEY (video_id),
+    FOREIGN KEY (episode_number) REFERENCES pka_episode (number) ON DELETE CASCADE
+);
